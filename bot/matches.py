b79fbdefdaf21a9ae9e17867408bbf19bc54a0a4
BenBicknellCA <benabicknell@gmail.com>
2022-08-17 06:43:25 -0400

    fix sched \n

--- bot/matches.py
+++ bot/matches.py
@@ -16,6 +16,7 @@ today = now.strftime("%d/%m/%Y")
 urlbase = "https://tennisapi1.p.rapidapi.com/api/tennis/events/"
 url = urlbase + today
 
+live_url = "https://tennisapi1.p.rapidapi.com/api/tennis/events/live"
 
 payload = ""
 headers = {
@@ -29,7 +30,7 @@ def sched():
     response = requests.request("GET", url, data=payload, headers=headers)
     json = response.json()
     results = json["events"]
-    for events in results:
+    for count, events in enumerate(results):
         tournament = events["tournament"]
         category = tournament["category"]
         league = category["name"]
@@ -42,11 +43,15 @@ def sched():
                 homeplayer = hometeam["name"]
                 awayplayer = awayteam["name"]
                 match = str(homeplayer + "  -  " + awayplayer)
-                allmatches = allmatches + match + "\n"
+                count = 1
+                count = count + 1
+                results_count = len(hometeam)
+
+                if results_count > count:
+                    allmatches = allmatches + match + "\n"
+                else:
+                    allmatches = allmatches + match
     return allmatches
-
-
-live_url = "https://tennisapi1.p.rapidapi.com/api/tennis/events/live"
 
 
 def live():
