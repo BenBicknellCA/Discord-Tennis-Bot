df9924d2cbe0d50b4357820897dd5d7ccb314591
Ben <benabicknell@gmail.com>
2022-08-19 17:31:45 -0400

    wip

--- bot/Emma_status.py
+++ bot/Emma_status.py
@@ -1,19 +1,28 @@
+import datetime
 import os
-from datetime import datetime
 
+import pytz
 import requests
+from datetimerange import DateTimeRange
 from dotenv import load_dotenv
+from jsonmerge import merge
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+# range_start = datetime.datetime.today(pytz.timezone("US/Eastern"))
+# range_end = datetime.datetime.now(pytz.timezone("US/Eastern")) + datetime.timedelta(
+#     hours=4
+# )
 
-now = datetime.now()
-today = now.strftime("%d/%m/%Y")
+right_now = datetime.datetime.today()
+today = right_now.strftime("%d/%m/%Y")
+tomorrow = right_now + datetime.timedelta(hours=24)
+tomorrow = tomorrow.strftime("%d/%m/%Y")
 
 urlbase = "https://tennisapi1.p.rapidapi.com/api/tennis/events/"
 url = urlbase + today
-
+url_tomorrow = urlbase + tomorrow
 
 payload = ""
 headers = {
@@ -24,8 +33,12 @@ headers = {
 
 def emma():
     response = requests.request("GET", url, data=payload, headers=headers)
+    tomorrow_response = requests.request(
+        "GET", url_tomorrow, data=payload, headers=headers
+    )
     json = response.json()
-    results = json["events"]
+    tomorrow_json = tomorrow_response.json()
+    results = json["events"] + tomorrow_json["events"]
     not_start = "Emma is playing later today!"
     is_start = "Emma is playing right now!"
     finish_start = "Emma already played today"
@@ -40,24 +53,36 @@ def emma():
         league = category["name"]
         emma_home = events["homeTeam"]
         emma_away = events["awayTeam"]
-        if league == "WTA":
-            if emma_home["id"] == 258756 or emma_away["id"] == 258756:
-                status = events["status"]
-                is_started = status["type"]
-                if is_started == "finished":
-                    emma_played = True
-                    if is_started == "inprogress":
-                        emma_playing = True
-                        if is_started == "notstarted":
-                            emma_willplay = True
-    if emma_willplay:
-        return not_start
-    elif emma_played:
-        return finish_start
-    elif emma_playing:
-        return is_start
-    else:
-        return not_play
+        time_category = events["time"]
+        time_unix = time_category["currentPeriodStartTimestamp"]
+        time_match = datetime.datetime.fromtimestamp(
+            time_unix, datetime.timezone(datetime.timedelta(hours=5))
+        )
+        time_start = time_match.strftime("%d/%m/%Y")
+        print(time_start)
+        print(events)
+        time_range = DateTimeRange(today, today)
+        if time_start in time_range:
+            if league == "WTA":
+                if emma_home["id"] == 258756 or emma_away["id"] == 258756:
+                    status = events["status"]
+                    is_started = status["type"]
+                    if is_started == "finished":
+                        emma_played = True
+                        if is_started == "inprogress":
+                            emma_playing = True
+                            if is_started == "notstarted":
+                                emma_willplay = True
+        if emma_willplay:
+            return not_start
+        elif emma_played:
+            return finish_start
+        elif emma_playing:
+            return is_start
+        else:
+            return not_play
 
+
+print(emma())
 
 # EMMA ID - 258756
