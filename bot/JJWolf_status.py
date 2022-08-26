8f435b489992bd55693d806bbbfb4640ab244d05
Ben <benabicknell@gmail.com>
2022-08-26 14:27:44 -0400

    refactored JJ command to work with new API

--- bot/JJWolf_status.py
+++ bot/JJWolf_status.py
@@ -1,54 +1,87 @@
+import datetime
 import os
-from datetime import date
 
+import pytz
 import requests
+from datetimerange import DateTimeRange
 from dotenv import load_dotenv
+from jsonmerge import merge
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
+right_now = datetime.datetime.today()
+today = right_now.strftime("%d/%m/%Y")
+tomorrow = right_now + datetime.timedelta(hours=24)
+tomorrow = tomorrow.strftime("%d/%m/%Y")
+
+urlbase = "https://tennisapi1.p.rapidapi.com/api/tennis/events/"
+url = urlbase + today
+url_tomorrow = urlbase + tomorrow
+
+payload = ""
+headers = {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": "tennisapi1.p.rapidapi.com",
+}
+
 
 def JJBot():
-
-    headers = {
-        "X-RapidAPI-Host": "tennis-live-data.p.rapidapi.com",
-        "X-RapidAPI-Key": API_KEY,
-    }
-    urlbase = "https://tennis-live-data.p.rapidapi.com/matches-by-date/"
-    # userdate = input("YYYY-MM-DD")
-    today = str(date.today())
-    url = urlbase + today
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, data=payload, headers=headers)
+    tomorrow_response = requests.request(
+        "GET", url_tomorrow, data=payload, headers=headers
+    )
     json = response.json()
-    matchdone = False
-    winner = False
-    willplay = False
-    no = "JJ does not play today :("
-    all = json["results"]
-    for empty in all:
-        yes = "JJ is playing today!"
-        no = "JJ does not play today :("
-        done = "JJ already played, "
-        win = "he won!"
-        lose = "mulletboy lost"
-        matches = empty["matches"]
-        for allmatch in matches:
-            result = allmatch["result"]
-            if allmatch["home_id"] == 1262886 or allmatch["away_id"] == 1262886:
-                if allmatch["status"] == "finished":
-                    matchdone = True
-                    if result["winner_id"] == 1262886:
-                        winner = True
-                else:
-                    willplay = True
-    if willplay:
-        return yes
-    elif matchdone and winner:
-        return done + win
-    elif matchdone and not winner:
-        return done + lose
-    else:
-        return no
+    tomorrow_json = tomorrow_response.json()
+    all_json = merge(json, tomorrow_json)
+    results = all_json["events"]
+    not_start = "JJ is playing later today!"
+    is_start = "JJ is playing right now!"
+    finish_start = "JJ already played today"
+    not_play = "JJ does not play today"
+    JJ_played = False
+    JJ_playing = False
+    JJ_willplay = False
+
+    for events in results:
+        tournament = events["tournament"]
+        category = tournament["category"]
+        league = category["name"]
+        JJ_home = events["homeTeam"]
+        JJ_away = events["awayTeam"]
+        time_category = events["time"]
+        time_unix = time_category["currentPeriodStartTimestamp"]
+        scheduled_unix = events["startTimestamp"]
+
+        try:
+            time_match = datetime.datetime.fromtimestamp(time_unix).astimezone(
+                pytz.timezone("US/Eastern")
+            )
+        except TypeError:
+            time_match = datetime.datetime.fromtimestamp(scheduled_unix).astimezone(
+                pytz.timezone("US/Eastern")
+            )
+
+        time_range = DateTimeRange(today, today)
+        if league == "WTA":
+            if time_match in time_range:
+                if JJ_home["id"] == 210479 or JJ_away["id"] == 210479:
+                    status = events["status"]
+                    is_started = status["type"]
+                    if is_started == "finished":
+                        JJ_played = True
+                        if is_started == "inprogress":
+                            JJ_playing = True
+                            if is_started == "notstarted":
+                                JJ_willplay = True
+        if JJ_willplay:
+            return not_start
+        elif JJ_played:
+            return finish_start
+        elif JJ_playing:
+            return is_start
+        else:
+            return not_play
 
 
-# NEW JJ ID - 210479
+# JJ ID -210479
